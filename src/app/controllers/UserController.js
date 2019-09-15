f53918b42c07d207181be1ffea009d06df6ee4a2
Allan Carvalho <allandcarv@gmail.com>
2019-09-15 18:30:28 -0300

    Add Input Data Validation at UserController (store)

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,7 +1,26 @@
+import * as Yup from 'yup';
+
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .min(6)
+        .required(),
+      confirmPassword: Yup.string()
+        .required()
+        .oneOf([Yup.ref('password')]),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Data Input Validation failed.' });
+    }
+
     const userExists = await User.findOne({ where: { email: req.body.email } });
 
     if (userExists) {
